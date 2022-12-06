13d6f5520aa02b3b6179d9604461c6e513e87d01
ahmedjacki <scofishop@gmail.com>
2022-12-07 02:22:10 +0300

    Ad d the main project structure

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+changeColorButton = document.querySelector('#change-color');
+box = document.querySelector('.box');
+
+changeColorButton.onclick = ()=>{
+    document.body.style.background = "red";
+    // alert('color has changed');
+}
